fb33690e2218d8954184a6900ee033fc81c898fb
ivoderuever <ivoderuever@gmail.com>
2021-07-18 17:42:59 +0200

    Add a basic layout for the randomizer

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,61 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view></router-view>
   </div>
 </template>
 
+<script>
+export default {
+
+}
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url("https://use.typekit.net/eqp3quc.css");
+
+:root {
+  --bg: #0c0c0c;
+  --font: #f0f0f0;
+
+  --primary: #dd3e61;
 }
 
-#nav {
-  padding: 30px;
+html, body {
+  height: unset;
+  width: unset;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+body {
+  background-color: var(--bg);
+  background: url(./assets/bg.png) no-repeat fixed center center / cover;
+  color: var(--font);
+  padding: 0;
+  margin: 0;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  font-family: sofia-pro;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+
+  .center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+
+.btn {
+  background-color: #FA9305;
+  border: 1px solid #853605;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+  font-size: 18px;
+  border-radius: 1px;
+  padding: 5px 10px;
+  cursor: pointer;
+  font-family: inherit;
+
+  &:hover {
+    background-color: #F08003;
   }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,133 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <h1>Stardew Valley romance picker</h1>
+    </header>
+    <div class="content center">
+        <div v-if="gender != null">
+
+        </div>
+        <div class="candidates" v-if="gender == null">
+            <h2>Which group of candidates do you want to get randomized result from?</h2>
+            <div class="gender-btn">
+                <button class="btn">All</button>
+                <button class="btn">Bachelors</button>
+                <button class="btn">Bachelorettes</button>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      //male = 0
+      //female = 1
+      //unknown = 2
+      vilagers: [
+        {
+          name: "Alex",
+          gender: 0,
+          image: 1,
+        },
+        {
+          name: "Elliott",
+          gender: 0,
+          image: 2,
+        },
+        {
+          name: "Harvey",
+          gender: 0,
+          image: 3,
+        },
+        {
+          name: "Sam",
+          gender: 0,
+          image: 4,
+        },
+        {
+          name: "Sabastian",
+          gender: 0,
+          image: 5,
+        },
+        {
+          name: "Shane",
+          gender: 0,
+          image: 6,
+        },
+        {
+          name: "Abigail",
+          gender: 1,
+          image: 7,
+        },
+        {
+          name: "Emily",
+          gender: 1,
+          image: 8,
+        },
+        {
+          name: "Haley",
+          gender: 1,
+          image: 9,
+        },
+        {
+          name: "Leah",
+          gender: 1,
+          image: 10,
+        },
+        {
+          name: "Maru",
+          gender: 1,
+          image: 11,
+        },
+        {
+          name: "Penny",
+          gender: 1,
+          image: 12,
+        },
+        {
+          name: "Krobus",
+          gender: 2,
+          image: 13,
+        },
+      ],
+        gender: null,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+header {
+  padding: 10px 10px;
+  h1 {
+    margin: 0;
+    font-size: 24px;
+    font-weight: 500;
   }
 }
-</script>
+
+.home {
+  height: 100vh;
+  width: 100%;
+
+    h2 {
+        display: block;
+        margin: 0;
+    }
+  .candidates {
+      padding: 10px 0px;
+      padding-top: 300px;
+      .gender-btn {
+          display: flex;
+          justify-content: space-between;
+          width: 300px;
+          margin: 0 auto;
+      padding: 10px 0px;
+          display: flex;
+      }
+  }
+}
+</style>
